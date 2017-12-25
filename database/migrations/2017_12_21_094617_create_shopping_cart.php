4fca56d8359e91b186ff0a78b86f0032b313b986
Sheng <sheng@wabow.com>
2017-12-25 09:57:08 +0800

    create cart migration

---
+++ database/migrations/2017_12_21_094617_create_shopping_cart.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShoppingCart extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(114);
+        Schema::create('shopping_carts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cart_no');
+            $table->longText('context');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shopping_carts');
+    }
+}
